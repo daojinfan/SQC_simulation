cd833d5a2b60e86ccea820e90345332c9b238395
daojinfan <djfan@mail.ustc.edu.cn>
2026-07-16 18:06:21 +0800

    chore: bind stage51 evolution artifact source

--- src/sqvm/evolution/__init__.py
+++ src/sqvm/evolution/__init__.py
@@ -6,15 +6,19 @@ from sqvm.evolution.input import load_stage5_input
 from sqvm.evolution.models import FormalScaleQualificationRequired
 from sqvm.evolution.physics import angular_rad_per_ns, evolve_stage5_scenario, zoh_edges
 from sqvm.evolution.stage51_authority import admit_verified_control
+from sqvm.evolution.stage51_artifacts import (
+    run_verified_control_evolution, verify_stage51_evolution_artifact,
+)
 from sqvm.evolution.stage51_context import production_stage51_physics_context
 from sqvm.evolution.stage51_coefficients import (
     build_evolution_coefficient_plan, publish_evolution_coefficient_artifact,
     verify_evolution_coefficient_artifact, verify_evolution_coefficient_staging,
 )
 from sqvm.evolution.stage51_models import (
-    EvolutionCoefficientPlan, Stage51EvolutionError, Stage51FailureCode,
-    Stage51PhysicsContext, VerifiedCoefficientHandle,
+    EvolutionCoefficientPlan, Stage51EvolutionArtifactSet, Stage51EvolutionError,
+    Stage51FailureCode, Stage51PhysicsContext, VerifiedCoefficientHandle,
+    VerifiedEvolutionHandle,
 )
 from sqvm.evolution.stage51_physics import phase_invariant_overlap, run_stage51_numerical_kernel
 
-__all__ = ["EvolutionCoefficientPlan", "FormalScaleQualificationRequired", "Stage51EvolutionError", "Stage51FailureCode", "Stage51PhysicsContext", "VerifiedCoefficientHandle", "admit_stage5_config_paths", "admit_verified_control", "angular_rad_per_ns", "build_evolution_coefficient_plan", "evolve_stage5_scenario", "load_stage5_input", "phase_invariant_overlap", "production_stage51_physics_context", "publish_evolution_coefficient_artifact", "run_stage5_evolution", "run_stage51_numerical_kernel", "verify_evolution_coefficient_artifact", "verify_evolution_coefficient_staging", "zoh_edges"]
+__all__ = ["EvolutionCoefficientPlan", "FormalScaleQualificationRequired", "Stage51EvolutionArtifactSet", "Stage51EvolutionError", "Stage51FailureCode", "Stage51PhysicsContext", "VerifiedCoefficientHandle", "VerifiedEvolutionHandle", "admit_stage5_config_paths", "admit_verified_control", "angular_rad_per_ns", "build_evolution_coefficient_plan", "evolve_stage5_scenario", "load_stage5_input", "phase_invariant_overlap", "production_stage51_physics_context", "publish_evolution_coefficient_artifact", "run_stage5_evolution", "run_stage51_numerical_kernel", "run_verified_control_evolution", "verify_evolution_coefficient_artifact", "verify_evolution_coefficient_staging", "verify_stage51_evolution_artifact", "zoh_edges"]
